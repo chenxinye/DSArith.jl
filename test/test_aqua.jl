7d164ab65bad0626065c3dea93e4d163539f1b52
chenxinye <cclcquant@yahoo.com>
2026-06-23 18:23:54 +0200

    fix workflows

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,5 +3,7 @@ using Aqua
 using DSArith
 
 @testset "Aqua" begin
-    Aqua.test_all(DSArith; ambiguities=false) # Ambiguities are noisy due to Base method overloading for Real interoperability.
+    # ambiguities=false: Real interoperability overloads trigger noisy ambiguity reports.
+    # unbound_args=false: DSArith intentionally uses parametric constructor/context signatures.
+    Aqua.test_all(DSArith; ambiguities=false, unbound_args=false)
 end
